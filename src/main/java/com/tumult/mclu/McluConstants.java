e80b7ab924e5c2a6fde7d7c0e548bbde37eadc1b
panakillustrations <panakillustrations@gmail.com>
2024-09-19 13:32:06 -0600

    Cleaned up CustomAttributeHudOverlay

---
+++ src/main/java/com/tumult/mclu/McluConstants.java
@@ -0,0 +1,9 @@
+package com.tumult.mclu;
+
+import net.minecraft.resources.ResourceLocation;
+
+public class McluConstants {
+    public static final String MOD_ID = "mclu";
+    public static final ResourceLocation MOD_ICONS_ATLAS = new ResourceLocation(MOD_ID, "textures/atlas/gui.png");
+
+}
---
+++ src/main/java/com/tumult/mclu/client/gui/icons/SpriteUploader.java
@@ -0,0 +1,29 @@
+package com.tumult.mclu.client.gui.icons;
+
+import com.tumult.mclu.McluConstants;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.resources.TextureAtlasHolder;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpriteUploader extends TextureAtlasHolder {
+    static final ResourceLocation GUI_ATLAS = new ResourceLocation(McluConstants.MOD_ID, "gui");
+
+    SpriteUploader(TextureManager textureManager) {
+        super(Minecraft.getInstance().getTextureManager(), McluConstants.MOD_ICONS_ATLAS, GUI_ATLAS);
+    }
+
+    @Override
+    public TextureAtlasSprite getSprite(ResourceLocation location) {
+        return super.getSprite(location);
+    }
+
+    public TextureAtlas getAtlas() {
+        return super.textureAtlas;
+    }
+}
